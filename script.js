a8decf87e12909edcaaa8e715e0daa220c259817
Forkeh <forkeh@gmail.com>
2024-11-12 18:26:09 +0100

    got collision with enemy working

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 
 window.addEventListener("load", init);
 
+// ****** Model ******
+// #region model
+
 const player = {
     x: 100,
     y: 100,
     speed: 200, // px/s
-    height: 40,
     width: 32,
+    height: 40,
 };
 
 const controls = {
@@ -17,20 +20,13 @@ const controls = {
     right: false,
 };
 
-let lastTime = 0;
-
-function init() {
-    requestAnimationFrame(tick);
-
-    document.addEventListener("keydown", keyPressed);
-    document.addEventListener("keyup", keyPressed);
-}
-
-function displayPlayer() {
-    const playerPos = document.querySelector("#player");
-
-    playerPos.style.translate = `${player.x}px ${player.y}px`;
-}
+const enemy = {
+    x: 200,
+    y: 100,
+    speed: 0, // px/s
+    width: 32,
+    height: 40,
+};
 
 function movePlayer(deltaTime) {
     const newPos = {
@@ -57,6 +53,7 @@ function movePlayer(deltaTime) {
     }
 }
 
+// Checks edges of game field
 function canMove(player, position) {
     if (position.y > 320 - player.height || position.y < 0) {
         return false;
@@ -65,6 +62,40 @@ function canMove(player, position) {
         return false;
     }
     return true;
+}
+
+function isColliding(rectA, rectB) {
+    // determine if the two rectangles are colliding, i.e. overlapping
+    // e.g. by using the AABB (Axis-Aligned Bounding Box) method
+
+    // Check if the right side of rectA is to the left of the left side of rectB
+    const rightOfLeft = rectA.x + rectA.width > rectB.x;
+
+    // Check if the left side of rectA is to the right of the right side of rectB
+    const leftOfRight = rectA.x < rectB.x + rectB.width;
+
+    // Check if the bottom side of rectA is above the top side of rectB
+    const bottomOfTop = rectA.y + rectA.height > rectB.y;
+
+    // Check if the top side of rectA is below the bottom side of rectB
+    const topOfBottom = rectA.y < rectB.y + rectB.height;
+
+    // If all conditions are true, the rectangles are colliding
+    return rightOfLeft && leftOfRight && bottomOfTop && topOfBottom;
+}
+
+// #endregion model
+
+// ****** Controller ******
+// #region controller
+
+let lastTime = 0;
+
+function init() {
+    requestAnimationFrame(tick);
+
+    document.addEventListener("keydown", keyPressed);
+    document.addEventListener("keyup", keyPressed);
 }
 
 function keyPressed(event) {
@@ -88,7 +119,43 @@ function tick(time) {
     const deltaTime = (time - lastTime) / 1000;
     lastTime = time;
 
+    // ignore the deltaTime and accurate frame calculation - just move 1 pixel each frame
     requestAnimationFrame(tick);
+
+    // move
     movePlayer(deltaTime);
+
+    // Check for collision with enemy
+    console.log("COLLISON:", isColliding(player, enemy));
+
+    if (isColliding(player, enemy)) {
+        player.collision = true;
+        enemy.collision = true;
+    } else {
+        player.collision = false;
+        enemy.collision = false;
+    }
+
+    // update display
     displayPlayer();
+    displayEnemy();
 }
+
+// #endregion controller
+
+// ****** View ******
+// #region view
+
+function displayPlayer() {
+    const playerPos = document.querySelector("#player");
+
+    playerPos.style.translate = `${player.x}px ${player.y}px`;
+}
+
+function displayEnemy() {
+    const enemyPos = document.querySelector("#enemy");
+
+    enemyPos.style.translate = `${enemy.x}px ${enemy.y}px`;
+}
+
+// #endregion view
